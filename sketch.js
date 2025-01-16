e37e30f7a196b3daa35093bf7429c1838de7246d
jinheeyi <jyi111@aucklanduni.ac.nz>
2025-01-16 17:48:55 +1300

    Update sketch.js
    added lines coding

--- sketch.js
+++ sketch.js
@@ -4,22 +4,25 @@ function setup() {
 }
 
 function draw() {
- 
+  
   fill(0, 10); 
   rect(0, 0, width, height);
 
-  
-  let size = random(20, 100); 
-  let x = random(width); 
-  let y = random(height); 
 
+  let size = random(20, 100); 
+  let x = random(width);
+  let y = random(height); 
 
   fill(random(255), random(255), random(255), 150); 
   if (random() < 0.5) {
     ellipse(x, y, size, size);
   } else {
-    rect(x, y, size, size); 
+    rect(x, y, size, size);
   }
+
+ 
+  stroke(255, random(200)); 
+  line(mouseX, mouseY, random(width), random(height));
 
 
   if (mouseIsPressed) {
