1c236a13a6589b3dcdebbc3dfbd7be375ae4953c
jinheeyi <jyi111@aucklanduni.ac.nz>
2025-01-16 17:44:06 +1300

    Update sketch.js
    changed the few errors

--- sketch.js
+++ sketch.js
@@ -4,24 +4,26 @@ function setup() {
 }
 
 function draw() {
-
+ 
   fill(0, 10); 
   rect(0, 0, width, height);
+
   
   let size = random(20, 100); 
   let x = random(width); 
-  let y = random(height);
+  let y = random(height); 
+
 
   fill(random(255), random(255), random(255), 150); 
   if (random() < 0.5) {
     ellipse(x, y, size, size);
-  }else{
+  } else {
     rect(x, y, size, size); 
   }
 
-  
+
   if (mouseIsPressed) {
     fill(random(255), random(255), random(255), 200); 
-    ellipse(mouseX + random(-10, 10), mouseY + random(-10, 10), 10, 10); 
+    ellipse(mouseX + random(-10, 10), mouseY + random(-10, 10), 10, 10);
   }
 }
